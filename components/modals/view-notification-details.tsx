9ad3986519a6bb6d8657f7a9c4a69189b0874088
exclusive <exclusiveadxe@gmail.com>
2026-01-12 10:57:45 +0100

    removed ip from notification details

--- components/modals/view-notification-details.tsx
+++ components/modals/view-notification-details.tsx
@@ -90,7 +90,7 @@ export default function ViewNotificationDetails({
           {/* Details Grid */}
           <div className="grid gap-4">
             {/* IP Address */}
-            {ip && (
+            {/* {ip && (
               <div className="flex items-center gap-3 p-3 bg-muted/30 rounded-lg">
                 <Globe className="w-5 h-5 text-muted-foreground flex-shrink-0" />
                 <div className="min-w-0 flex-1">
@@ -98,7 +98,7 @@ export default function ViewNotificationDetails({
                   <p className="text-foreground font-mono text-sm">{ip}</p>
                 </div>
               </div>
-            )}
+            )} */}
 
             {/* Login Time */}
             {loginTime && (
